871b759e9417f56607bcf35fe9d71f8181e2cb71
oilprint <irynakozlova.k@gmail.com>
2023-12-06 23:58:05 +0200

    added edit and archive event

--- js/main.js
+++ js/main.js
@@ -24,13 +24,16 @@ console.log(allTasks);
 const msgType = {
   add: 'add-task',
   remove: 'remove-task',
-  done: 'done-task'
+  done: 'done-task',
+  archive: 'archive-task'
 };
 
 const msgText = {
   add: 'Додано нове завдання',
   remove: 'Завдання видалено',
-  done: 'Завдання виконано'
+  done: 'Завдання виконано',
+  archive: 'Завдання додано до архіву',
+  unArchive: 'Завдання додано до архіву',
 };
 const yearInMs = 365 *24 * 3600 *1000;
 
@@ -94,7 +97,7 @@ function checkTime(target, message) {
   const currentTime = convertStringTimeToMS(`${new Date().getHours()}:${new Date().getMinutes()}`);
   const inputDate = (myForm.taskDate.value.replaceAll('0', '')) || 0;
   const currentDate = (`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`).replaceAll('0', '');
-  if (inputDate === currentDate && inputTime < currentTime + 30 * 6000) {
+  if (inputDate === currentDate && inputTime < currentTime + 30 * 60000) {
     console.log('alert');
     addFieldError(target, messages.errorTime);
   } else if (!inputDate) {
@@ -106,13 +109,12 @@ function checkTime(target, message) {
 };
 
 function checkDate(target, message) {
-  const inputDate = Date.parse(target.value) - (3 * 3600 * 1000);
+  const inputDate = Date.parse(target.value) ;
   const currentTime = convertStringTimeToMS(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}`);
   const currentDate = Date.now() - currentTime;
   const timeDiff = convertStringTimeToMS(myForm.taskTime.value) - currentTime;
 
   console.log(Date.parse(target.value));
-  console.log(currentDate);
   if (inputDate < currentDate) {
     addFieldError(target, message.smallDate);
   } else if (inputDate - currentDate > yearInMs ) {
@@ -147,7 +149,8 @@ function createTask() {
     id: Math.floor(Math.random() * 10000),
     text: myForm.taskText.value,
     dateTime: `${myForm.taskDate.value}T${myForm.taskTime.value}`,
-    taskStatus: 'new',
+    inArchive: false,
+    taskStatus: 'new'
   };
 
   myForm.taskCategory.forEach(radio => {
@@ -194,11 +197,12 @@ function addTaskToPage(arr) {
     const taskTime = getRemainingTime(el.dateTime);
     const expiredClass = taskTime === 'Завдання протерміноване' ? 'expired' : '';
     const doneClass = el.taskStatus === 'done' ? el.taskStatus : '';
+    const archiveClass = el.inArchive === true ? 'archive' : '';
     const checked = el.taskStatus === 'done' ? 'checked' : '';
     
     taskList.insertAdjacentHTML('afterbegin', `
       <li class="task-list__item">
-      <div class="task ${el.category} ${expiredClass} ${doneClass}">
+      <div class="task ${el.category} ${expiredClass} ${doneClass} ${archiveClass}">
       <div class="task__head">
           <div class="task__check">
               <input
@@ -212,13 +216,15 @@ function addTaskToPage(arr) {
               <div class="task__name">${el.text}</div>
           </div>
           <div class="task__actions">
-              <button class="btn btn--icon btn--red task__btn task__btn--remove " type="button" title="Видалити"></button>
-          </div>
+          <button class="btn btn--icon btn--orange task__btn task__btn--edit" type="button" title="Редагувати"></button>
+          <button class="btn btn--icon btn--red task__btn task__btn--remove" type="button" title="Видалити"></button>
+        </div>
       </div>
       <div class="task__info">
           <div class="task__category" title="${category}"></div>
           <div class="task__date">${taskTime} днів </div>
       </div>
+      <button class="btn btn--icon btn--gray task__btn task__btn--archive" type="button" title="Архівувати/Вилучити"></button>
   </div>
 </li>
   `)
@@ -302,6 +308,17 @@ function changeTaskStatus(id) {
   addTaskToPage(allTasks);
 };
 
+function changeArchiveStatus(id) {
+  allTasks.forEach(item => { 
+    if (item.id === +id) {
+      item.inArchive = item.inArchive === true ? false : true;
+      addMessage(msgType.archive, msgText.archive);
+    }
+  });
+  updateLocalStorage(allTasks);
+  addTaskToPage(allTasks);
+};
+
 function removeTask(id) {
   allTasks.forEach(item => { 
     if (item.id === +id) {
@@ -310,28 +327,97 @@ function removeTask(id) {
       addMessage(msgType.remove, msgText.remove);
     }
   });
-  updateLocalStorage(allTasks);
+  updateLocalStorage(allTasks);    
   addTaskToPage(allTasks);
-}
+};
+
+function textOfTask(id) {
+  let text;
+  allTasks.forEach(item => { 
+    if (item.id === +id) {
+      text = item.text;   
+    }
+  });
+  return text;
+};
+
+
 
 document.addEventListener('click', (e) => {
   const target = e.target;
 
   if (target.classList.contains('add-task__btn')) {
     handlerAddTaskBtn(e);
-  }
+  };
 
   if (target.classList.contains('checkbox-input')) {
-    console.log(target.id);
     target.closest('.task').classList.toggle('done');
     changeTaskStatus(target.id);
     console.log(allTasks);
-  }
+  };
 
   if (target.classList.contains('task__btn--remove')) {
     const id = +target.closest('.task').querySelector('.checkbox-input').id;
     removeTask(id);
-  }
+  };
+
+  if (target.classList.contains('task__btn--edit')) {
+    
+    const id = +target.closest('.task').querySelector('.checkbox-input').id;
+
+    const text = textOfTask(id);
+
+    target.closest('.task').insertAdjacentHTML('afterbegin', `
+      <form class="form form--edit" name="editTask">
+        <div class="add-task">
+          <div class="add-task__group">
+            <textarea class="form-field" id="task-text" name="taskText">${text}</textarea>
+            <div class="form-error-msg"></div>
+        </div>
+        <div class="d-flex j-end g-10">
+            <button class="btn btn--sm btn--gray js_reject-changes" type="button">Відхилити</button>
+            <button class="btn btn--sm btn--green js_save-changes" type="button">OK</button>
+        </div>
+      </div>
+    </form>
+    `);
+    const editForm = target.closest('.task').querySelector('.form');
+    addFormValidation(editForm);
+  };
+
+  if (target.classList.contains('js_save-changes')) {
+    const id = +target.closest('.task').querySelector('.checkbox-input').id;
+    const editForm = target.closest('.task').querySelector('.form');
+
+    const editText = editForm.querySelector('#task-text').value;
+    console.log(editText);
+
+    allTasks.forEach(item => { 
+    if (item.id === +id) {
+      item.text = editText;
+    }
+  });
+    updateLocalStorage(allTasks);
+    addTaskToPage(allTasks);
+    target.closest('.add-task').remove();
+  };
+
+  if (target.classList.contains('js_reject-changes')) {
+    target.closest('.add-task').remove();
+  };
+
+  if (target.classList.contains('task__btn--archive')) {
+    const id = +target.closest('.task').querySelector('.checkbox-input').id;
+    console.log(id);
+
+    target.closest('.task').classList.toggle('archive');
+    changeArchiveStatus(id);
+  };
+
+
+  
+
+
 });
 
 
@@ -340,5 +426,7 @@ document.addEventListener('click', (e) => {
 // 3. додати дію для кнопок "ок" та "відміна"
 // 4. додави фільтри та лічильник завдань
 // 4. валидація фільтрів 
+
+
 
 
